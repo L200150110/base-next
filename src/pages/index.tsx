a14060574174ed40aa4235bc13089895a98ee721
Riski Agung <riski97@gmail.com>
2023-10-14 21:43:45 +0700

    fix bug

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Login from "./login";
+
+function Pages() {
+  return <Login />;
+}
+
+export default Pages;
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "bulma/css/bulma.css";
+
+const Login = () => {
+  return (
+    <section className="hero has-background-grey-light is-fullheight is-fullwidth">
+      <div className="hero-body">
+        <div className="container">
+          <div className="columns is-centered">
+            <div className="column is-4-desktop">
+              <form className="box">
+                <div className="field">
+                  <label className="label">Email or username</label>
+                  <div className="controls">
+                    <input
+                      type="text"
+                      className="input"
+                      placeholder="Email or username"
+                    />
+                  </div>
+                </div>
+                <div className="field mt-5">
+                  <label className="label">Password</label>
+                  <div className="controls">
+                    <input
+                      type="password"
+                      className="input"
+                      placeholder="**********"
+                    />
+                  </div>
+                </div>
+                <div className="field mt-5">
+                  <button className="button is-success is-fullwidth">
+                    Login
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
